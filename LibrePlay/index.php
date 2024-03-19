9ac65c0a364340f8ee92bbf0c4a270c7b3ed48f2
André Luiz Cecato Justus <83723864+andrececato@users.noreply.github.com>
2024-03-19 16:20:33 -0300

    Add files via upload

---
+++ LibrePlay/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LibrePlay</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <ul class="nav-list">
+            <li>Assista agora</li>
+            <li>Filmes</li>
+            <li>Séries</li>
+            <li>Esporte</li>
+            <li>Kids</li>
+            <li>Biblioteca</li>
+            <li>O</li>
+        </ul>
+    </header>
+
+    <main>
+        <div class="section">
+            <h1 class="title">Top filmes</h1>
+            <br>
+            <div class="banner-list">
+                <div class="banner"></div>
+                <div class="banner"></div>
+                <div class="banner"></div>
+                <div class="banner"></div>
+                <div class="banner"></div>
+            </div>
+        </div>
+    </main>
+    
+    <footer></footer>
+</body>
+</html>
